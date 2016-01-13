a035da757a44f876f12b38b181fc3f13003f3eb3
lyricdon <lyricdon@hotmail.com>
2016-01-13 16:08:58 +0800

    Initial Commit

---
+++ PCDialogBoxView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PCDialogBoxView
+//
+//  Created by lyricdon on 16/1/13.
+//  Copyright © 2016年 lyricdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PCDialogBoxView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PCDialogBoxView
+//
+//  Created by lyricdon on 16/1/13.
+//  Copyright © 2016年 lyricdon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
